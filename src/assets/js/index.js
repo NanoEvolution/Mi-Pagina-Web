984ae0960ae516d787c581e23c96f74da2fa3ef4
Daniel <danielpro1209@gmail.com>
2023-12-08 01:39:41 +0100

    Inicio,navbar y preparacion de la pagina hecho

---
+++ src/assets/js/index.js
@@ -0,0 +1,25 @@
+// Función para desplazarse suavemente al principio de la página
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+}
+
+// Función que se ejecuta cuando se desplaza la página
+window.onscroll = function() {
+    scrollFunction();
+};
+
+// Función para mostrar u ocultar el botón de regreso al inicio
+function scrollFunction() {
+    var goTopBtn = document.querySelector('.go-top-btn');
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        goTopBtn.classList.add('show');
+    } else {
+        goTopBtn.classList.remove('show');
+    }
+}
+
+// Asignar la función scrollToTop al hacer clic en el botón de regreso al inicio
+document.querySelector('.go-top-btn').addEventListener('click', scrollToTop);
